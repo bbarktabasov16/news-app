ce6c3133fc3daff467b7377ebb66f5f3056e803a
Bekzat Baryktabasov <bbarktabasov16@gmail.com>
2024-05-02 23:35:45 +0600

    add context and change theme

--- src/components/NewsByFilters/NewsByFilters.tsx
+++ src/components/NewsByFilters/NewsByFilters.tsx
@@ -42,7 +42,10 @@ const NewsByFilters = () => {
 
   return (
     <section className={styles.section}>
-      <NewsFilters filters={filters} changeFilter={changeFilter} />
+      <NewsFilters
+        filters={filters}
+        changeFilter={changeFilter}
+      />
 
       <PaginationWrapper
         top
--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,9 +4,10 @@ import styles from "./styles.module.css";
 interface Props {
   children: React.ReactElement;
   step?: number;
+  isDark: boolean;
 }
 
-const Slider = ({ children, step = 150 }: Props) => {
+const Slider = ({ children, step = 150, isDark }: Props) => {
   const sliderRef = useRef<HTMLElement | null>(null);
 
   const scrollLeft = () => {
@@ -15,11 +16,11 @@ const Slider = ({ children, step = 150 }: Props) => {
   };
 
   const scrollRight = () => {
-		if (!sliderRef.current) return;
+    if (!sliderRef.current) return;
     sliderRef.current.scrollLeft += step;
   };
   return (
-    <div className={styles.slider}>
+    <div className={`${styles.slider} ${isDark ? styles.dark : styles.light}`}>
       <button onClick={scrollLeft} className={styles.arrow}>{`<`}</button>
       {React.cloneElement(children, { ref: sliderRef })}
       <button onClick={scrollRight} className={styles.arrow}>{`>`}</button>
